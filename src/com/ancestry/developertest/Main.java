4f93f660b9d352a43a6fb2ca0ae469acd0f217cf
Josh Proulx <josh@joshproulx.com>
2015-01-08 12:02:59 -0700

    Step 1 - Initial Hello World command line app

---
+++ src/com/ancestry/developertest/Main.java
@@ -0,0 +1,9 @@
+package com.ancestry.developertest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Hello World
+        System.out.println("Hello World!");
+    }
+}
